99622c7f3d05519f39d25ad972a892b6c8cec91e
Dmitry Makov <dvmohner.2004@gmail.com>
2024-04-16 19:33:35 +0300

    Add cli.py, install prompt package

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+from brain_games import cli
+
+
 
 def main():
     print("Welcome to the Brain Games!")
@@ -6,3 +9,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+    cli.welcome_user()
