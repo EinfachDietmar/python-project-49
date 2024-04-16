f74e9c5a3599af721e2e1c13cb69e4454564745f
Dmitry Makov <dvmohner.2004@gmail.com>
2024-04-16 19:47:40 +0300

    Revert "Add cli.py, install prompt package"
    This reverts commit 99622c7f3d05519f39d25ad972a892b6c8cec91e.

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,7 +1,4 @@
 #!/usr/bin/env python3
-from brain_games import cli
-
-
 
 def main():
     print("Welcome to the Brain Games!")
@@ -9,4 +6,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-    cli.welcome_user()
