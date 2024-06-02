6a1d7f0e96e0aa1897a0fd2f2d932544e42c46c8
Dmitry Makov <dvmohner.2004@gmail.com>
2024-06-02 04:47:57 +0300

    Add brain_even.py, edit cli.py and edit brain_games.py

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,6 +1,8 @@
-import prompt
+import prompt  # type: ignore
 
 
 def welcome_user():
+    global name
     name = prompt.string('May I have your name? ')
     print(f'Hello, {name}!')
+    
---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+from brain_games import cli  # type: ignore
+from .brain_games import main
+import random
+
+
+def game_parity_check():
+    main()
+    print('Answer "yes" if the number is even, otherwise answer "no".')
+    i = 0
+    while i < 3:
+        random_number = random.randint(1, 101)
+        print(f'Question: {random_number}')
+        player_input = str(input('Your answer: ')).lower()
+        if random_number % 2 == 0 and player_input == 'yes':
+            i += 1
+            print('Correct!')
+        elif random_number % 2 != 0 and player_input == 'no':
+            i += 1
+            print('Correct!')
+        elif random_number % 2 == 1 and player_input == 'yes':
+            right_answer = 'no'
+            return print(f"""'{player_input}' is wrong answer ;(. Correct answer was '{right_answer}'.
+et's try again, {cli.name}""")
+        elif random_number % 2 == 0 and player_input == 'no':
+            right_answer = 'yes'
+            return print(f"""'{player_input}' is wrong answer ;(. Correct answer was '{right_answer}'.
+Let's try again, {cli.name}""")
+    print(f'Congratulations, {cli.name}!')
+
+
+if __name__ == '__main__':
+    game_parity_check()
